a1422715c08f0773fb3019b50edd48d3dadc6559
rafi-windows <rafi_n_o@github.com>
2020-12-08 06:37:39 +0700

    Menampilkan Data di Vue Js

--- src/views/post/Index.vue
+++ src/views/post/Index.vue
@@ -1,7 +1,79 @@
 <template>
-  <h1>Index</h1>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card border-0 rounded shadow">
+          <div class="card-body">
+            <h4>Data Post</h4>
+            <hr />
+            <router-link
+              :to="{ name: 'post.create' }"
+              class="btn btn-md btn-success"
+              >Tambah Post</router-link
+            >
+            <table class="table table-striped table-bordered mt-4">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Content</th>
+                  <th scope="col">Options</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(post, index) in posts" :key="index">
+                  <td>{{ post.title }}</td>
+                  <td>{{ post.content }}</td>
+                  <td class="text-center">
+                    <router-link
+                      :to="{ name: 'post.edit', params: { id: post.id } }"
+                      class="btn btn-sm btn-primary mr-1"
+                      >Edit</router-link
+                    >
+                    <button class="btn btn-sm btn-danger ml-1">Delete</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+export default {
+  setup() {
+    // reactive state
+    let posts = ref([]);
+
+    // mounted
+    onMounted(() => {
+      // get API from laravel backend
+      axios
+        .get("http://localhost:8000/api/post")
+        .then((response) => {
+          // assign state posts with response data
+          posts.value = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+    });
+
+    // return
+    return {
+      posts,
+    };
+  },
+};
 </script>
+
+<style>
+body {
+  background: lightgray;
+}
+</style>
