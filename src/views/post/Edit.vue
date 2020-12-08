48e5118cce77b9866c1ee9ab2354b2c5220983eb
rafi-windows <rafi_n_o@github.com>
2020-12-08 07:32:37 +0700

    Edit dan Update Data di Vue Js

--- src/views/post/Edit.vue
+++ src/views/post/Edit.vue
@@ -1,7 +1,113 @@
 <template>
-  <h1>Edit</h1>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card border-0 rounded shadow">
+          <div class="card-body">
+            <h4>Edit Post</h4>
+            <hr />
+            <form @submit.prevent="update">
+              <div class="form-group">
+                <label for="title" class="font-weight-bold">Title</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="post.title"
+                  placeholder="Masukkan Judul Post"
+                />
+                <!-- Validation -->
+                <div v-if="validation.title" class="mt-2 alert alert-danger">
+                  {{ validation.title[0] }}
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="content" class="font-weight-bold">Content</label>
+                <textarea
+                  class="form-control"
+                  rows="4"
+                  v-model="post.content"
+                  placeholder="Masukkan Konten Post"
+                ></textarea>
+                <!-- Validation -->
+                <div v-if="validation.content" class="mt-2 alert alert-danger">
+                  {{ validation.content[0] }}
+                </div>
+              </div>
+              <button type="submit" class="btn btn-primary">Simpan</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { reactive, ref, onMounted } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import axios from "axios";
+
+export default {
+  setup() {
+    // state posts
+    const post = reactive({
+      title: "",
+      content: "",
+    });
+
+    // state validation
+    const validation = ref([]);
+
+    // vue router
+    const router = useRouter();
+
+    // vue route
+    const route = useRoute();
+
+    // mounted
+    onMounted(() => {
+      // get API from laravel backend
+      axios
+        .get(`http://localhost:8000/api/post/${route.params.id}`)
+        .then((response) => {
+          // assign state posts with response data
+          post.title = response.data.data.title;
+          post.content = response.data.data.content;
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+    });
+
+    // method update
+    function update() {
+      let title = post.title;
+      let content = post.content;
+
+      axios
+        .put(`http://localhost:8000/api/post/${route.params.id}`, {
+          title: title,
+          content: content,
+        })
+        .then(() => {
+          // redirect ke post index
+          router.push({
+            name: "post.index",
+          });
+        })
+        .catch((error) => {
+          // assign state validation with error
+          validation.value = error.response.data;
+        });
+    }
+
+    // return
+    return {
+      post,
+      validation,
+      router,
+      update,
+    };
+  },
+};
 </script>
